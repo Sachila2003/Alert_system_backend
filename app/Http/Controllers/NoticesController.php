c53fbec31d954f4a2033b8a43ef9c2ff4dba4760
Sachila2003 <sachilageeth1@gmail.com>
2026-03-23 09:44:21 +0530

    table structure done

---
+++ app/Http/Controllers/NoticesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notices;
+use Illuminate\Http\Request;
+
+class NoticesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Notices $notices)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Notices $notices)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Notices $notices)
+    {
+        //
+    }
+}
